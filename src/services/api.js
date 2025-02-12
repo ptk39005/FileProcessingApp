e66338ef58b271920e74dc1d4f93b18dbe70d4a9
Pratik <88044632+ptk39005@users.noreply.github.com>
2025-02-12 18:44:23 +0530

    redirection fixes

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,8 @@ import { storage } from "./firebaseConfig";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 // Create an Axios instance with base URL and common configurations
 const API = axios.create({
-   //baseURL: 'https://backend-env.eba-sgvmg8bp.ap-south-1.elasticbeanstalk.com/',
-  baseURL : "http://127.0.0.1:5000",
+   baseURL: 'https://backend-env.eba-sgvmg8bp.ap-south-1.elasticbeanstalk.com/',
+  //baseURL : "http://127.0.0.1:5000",
     headers: {
         'Content-Type': 'application/json',
     },
